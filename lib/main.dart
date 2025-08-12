3567b81addec28b8d3446dd531c76c8513ef467e
beyremagrebi <95504967+beyremagrebi@users.noreply.github.com>
2025-08-12 14:26:15 +0100

    fix version

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:erudaxis/core/config/app_launch.dart';
 import 'package:erudaxis/erudaxis.dart';
 import 'package:flutter/material.dart';
 
-/// MAIN APP  [BEYREM ]
+/// MAIN APP  [BEYREM]
 Future<void> main() async {
   await AppLaunch.preExecution();
   runApp(const Erudaxis());
